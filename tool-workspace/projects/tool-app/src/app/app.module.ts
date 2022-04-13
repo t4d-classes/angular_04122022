d2bc7984daaf59f1ba73f4f75057d1a665600809
Eric Greene <eric@t4d.io>
2022-04-13 10:06:04 -0400

    add layout and initial routing to tool app

--- tool-workspace/projects/tool-app/src/app/app.module.ts
+++ tool-workspace/projects/tool-app/src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { SharedLibModule } from 'shared-lib';
-import { ColorToolLibModule } from 'color-tool-lib';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHomeComponent } from './components/app-home/app-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHomeComponent
   ],
   imports: [
     BrowserModule,
     SharedLibModule,
-    ColorToolLibModule,
     AppRoutingModule
   ],
   providers: [],
